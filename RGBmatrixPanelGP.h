8e8b302767bad36bf2fa58abcc0b5a80e3c47dcf
3tawi <96850409+3tawi@users.noreply.github.com>
2022-03-31 20:17:55 +0000

    Update RGBmatrixPanelGP.h

--- RGBmatrixPanelGP.h
+++ RGBmatrixPanelGP.h
@@ -8,16 +8,16 @@
 
 typedef uint32_t PortType; // Formerly 'RwReg' but interfered w/CMCIS header
 
-class RGBMATRIXPANELGP : public Adafruit_GFX {
+class RGBmatrixPanelGP : public Adafruit_GFX {
 
 public:
-  RGBMATRIXPANELGP(uint8_t a, uint8_t b, uint8_t c, uint8_t clk, uint8_t lat,
+  RGBmatrixPanelGP(uint8_t a, uint8_t b, uint8_t c, uint8_t clk, uint8_t lat,
                  uint8_t oe, boolean dbuf, uint8_t *pinlist = NULL);
 
-  RGBMATRIXPANELGP(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t clk,
+  RGBmatrixPanelGP(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t clk,
                  uint8_t lat, uint8_t oe, boolean dbuf, uint8_t width = 32, uint8_t *pinlist = NULL);
 
-  RGBMATRIXPANELGP(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t clk,
+  RGBmatrixPanelGP(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t clk,
                  uint8_t lat, uint8_t oe, boolean dbuf, uint8_t width = 32, uint8_t *pinlist = NULL);
 
   void begin(void);
@@ -59,12 +59,7 @@ private:
 
   // Init/alloc code common to both constructors:
   void init(uint8_t rows, uint8_t a, uint8_t b, uint8_t c, uint8_t clk,
-            uint8_t lat, uint8_t oe, boolean dbuf, uint8_t width
-#if defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_ESP32)
-            ,
-            uint8_t *rgbpins
-#endif
-  );
+            uint8_t lat, uint8_t oe, boolean dbuf, uint8_t width, uint8_t *rgbpins);
 
   uint8_t _clk;       ///< RGB clock pin number
   uint8_t _lat;       ///< RGB latch pin number
